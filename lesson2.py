597446c10f8d6f1e0f04dcbaa953fba18024d793
yougebuding <42401339+yougebuding@users.noreply.github.com>
2022-03-05 23:18:22 +0800

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,18 @@
+a = input("请猜一个数：")
+print("你的输入是：",a)
+
+money = 20
+result = money * 2 +50
+print(result)
+
+a = 'hello,world'
+b = 1.414
+c = (1 != 2)
+d = (True and False)
+e = (True or False)
+print(a,b,c,d,e)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
